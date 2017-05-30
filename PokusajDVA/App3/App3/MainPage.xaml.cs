628e64cdaec87cfb1fe4f3c614ae24d92c4b1b7c
hsikic1 <hsikic1@etf.unsa.ba>
2017-05-31 00:40:16 +0200

    bismila

--- PokusajDVA/App3/App3/MainPage.xaml.cs
+++ PokusajDVA/App3/App3/MainPage.xaml.cs
@@ -12,6 +12,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using TutorFinderApp.View;
 
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
@@ -26,6 +27,11 @@ namespace TutorFinderApp
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        public void OnPageLoad(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(MainKlijent));
         }
     }
 }
